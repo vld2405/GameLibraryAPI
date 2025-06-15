3b4e4bc9a54e2a87079b411c542da1a854628ab8
Shrike002 <raduvisoiu.samsung@gmail.com>
2025-06-16 02:15:48 +0300

    Added repository classes for each entity

---
+++ GameLibraryAPI/GameLibrary.Database/Repositories/DeveloperRepository.cs
@@ -0,0 +1,47 @@
+﻿using GameLibrary.Database.Context;
+using GameLibrary.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.Database.Repositories
+{
+    public class DeveloperRepository : BaseRepository<Developer>
+    {
+        public DeveloperRepository(GameLibraryDatabaseContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<Developer?> GetDevelopersAsync(int id)
+        {
+            return await GetRecords().FirstOrDefaultAsync(nt => nt.Id == id);
+        }
+        public async Task<IEnumerable<Developer>> GetDevelopersAsync()
+        {
+            return await GetRecords().ToListAsync();
+        }
+        public async Task<(IEnumerable<Developer> developers, int totalCount)> GetDevelopersAsync(int? pageNumber = null, int? pageSize = null)
+        {
+            IQueryable<Developer> query = GetRecords();
+
+            var totalCount = await query.CountAsync();
+
+            if (pageNumber.HasValue && pageSize.HasValue && pageNumber > 0 && pageSize > 0)
+            {
+                query = query
+                    .Skip((pageNumber.Value - 1) * pageSize.Value)
+                    .Take(pageSize.Value);
+            }
+
+            var developers = await query.ToListAsync();
+
+            return (developers, totalCount);
+        }
+
+
+
+    }
+}
---
+++ GameLibraryAPI/GameLibrary.Database/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using GameLibrary.Database.Context;
+using GameLibrary.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.Database.Repositories
+{
+    public class UserRepository : BaseRepository<User>
+    {
+        public UserRepository(GameLibraryDatabaseContext dbContext) : base(dbContext)
+        {
+        }
+        public async Task<User?> GetUserAsync(int id)
+        {
+            return await GetRecords().FirstOrDefaultAsync(u => u.Id == id);
+        }
+        public async Task<IEnumerable<User>> GetUsersAsync()
+        {
+            return await GetRecords().ToListAsync();
+        }
+        public async Task<(IEnumerable<User> users, int totalCount)> GetUsersAsync(int? pageNumber = null, int? pageSize = null)
+        {
+            IQueryable<User> query = GetRecords();
+            var totalCount = await query.CountAsync();
+            if (pageNumber.HasValue && pageSize.HasValue && pageNumber > 0 && pageSize > 0)
+            {
+                query = query
+                    .Skip((pageNumber.Value - 1) * pageSize.Value)
+                    .Take(pageSize.Value);
+            }
+            var users = await query.ToListAsync();
+            return (users, totalCount);
+        }
+    }
+}
